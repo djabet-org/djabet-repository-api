11b85936a9d816c572226833fccf3ab1957ddbbe
Anderson Luiz <anderson.luiz@liferay.com>
2024-05-20 18:09:16 -0300

    send created roll as json

--- src/main/java/hello/service/SseService.java
+++ src/main/java/hello/service/SseService.java
@@ -1,7 +1,10 @@
 package hello.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import hello.Roll;
 
@@ -14,6 +17,9 @@ public class SseService {
 
     private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
 
+    @Autowired
+    private ObjectMapper mapper;
+
     public void addEmitter(SseEmitter emitter) {
         emitters.add(emitter);
         emitter.onCompletion(() -> emitters.remove(emitter));
@@ -23,7 +29,7 @@ public class SseService {
     public void sendEvents(Roll savedRoll) {
         for (SseEmitter emitter : emitters) {
             try {
-                emitter.send(savedRoll.toString());
+                emitter.send(mapper.writeValueAsString(savedRoll));
             } catch (IOException e) {
                 emitter.complete();
                 emitters.remove(emitter);
