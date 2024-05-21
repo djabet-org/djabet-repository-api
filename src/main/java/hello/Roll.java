1bf53a8cde0579a367852b22486b10c16b3a0d21
Anderson Luiz <anderson.luiz@liferay.com>
2024-05-21 16:28:28 -0300

    show created time as timestamp

--- src/main/java/hello/Roll.java
+++ src/main/java/hello/Roll.java
@@ -7,10 +7,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -37,8 +39,14 @@ public class Roll {
     private Integer roll;
     @JsonIgnore
     private Instant created = Instant.now();
+    @Transient
+    private String createdTime = created.toString();
     private float total_red_money = 0;
     private float total_white_money = 0;
     private float total_black_money = 0;
+
+    @Transient
+    @JsonProperty
+    private long createdTimeAsTimestamp = created.toEpochMilli();
 
 }
